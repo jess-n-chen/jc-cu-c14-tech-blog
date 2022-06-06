732908525d805f684ac8e4601426d9b5c0d2b613
Jessica Chen <jessicachen28@gmail.com>
2022-06-06 04:41:40 -0400

    Fix Comments Display and Updated Session Timeout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,6 +35,20 @@ router.get("/post/:id", withAuth, async (req, res) => {
       attributes: ["id", "title", "content", "created_at"],
       include: [
         {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
           model: User,
           attributes: ["username"],
         },
