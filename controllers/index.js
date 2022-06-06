e6027b2b6c549be0ba10b40c188044c1dd0845af
Jessica Chen <jessicachen28@gmail.com>
2022-06-06 04:53:57 -0400

    Code Refactor

--- controllers/index.js
+++ controllers/index.js
@@ -2,16 +2,16 @@
 const router = require("express").Router();
 
 // Import Required Files
-const apiRoutes = require("./api");
+const apiRoutes = require("./api/");
 const homeRoutes = require("./home-routes.js");
 const dashboardRoutes = require("./dashboard-routes.js");
-
-// Prefix API Endpoints with /api Path
-router.use("/api", apiRoutes);
 
 // Set Route for Other Paths
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
+
+// Prefix API Endpoints with /api Path
+router.use("/api", apiRoutes);
 
 // Return 404 Error For Requests to Non-Existent Endpoints
 router.use((req, res) => {
